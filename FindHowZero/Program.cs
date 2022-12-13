a4d224890b34e2f3dfc1349fb4ca068f4b4f143e
Kostya Vasilyev <kostiavasilev10@gamil.com>
2022-12-13 19:08:20 +0300

    add FindHowZero

---
+++ FindHowZero/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите элементы: ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+
+for (int i = 0; i < arr.Length; i++)
+{
+    if (arr[i] > 0)
+    {
+        count++;
+    }
+}
+
+Console.WriteLine($"Колличество элементов > 0 = {count}");
